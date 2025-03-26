5f75ba484613e03a0356025d32b27c2b2d279af2
rafitrhmn <rafitaufih10@gmail.com>
2025-03-26 14:33:41 +0700

    add data structure for Al-Matsurat Pagi Kubro

---
+++ lib/model/data_kubro_pagi.dart
@@ -0,0 +1,59 @@
+Map<String, dynamic> response = {
+  "status": "success",
+  "message": "Al-Matsurat Pagi Kubro",
+  "data": [
+    {
+      "no": 1,
+      "surah": "Taawudz",
+      "ayat": "1",
+      "dzikir": "أَعُوذُ بِاللَّهِ مِنَ الشَّيْطَانِ الرَّجِيمِ",
+      "latin": "A'udzubillaahis samii'il 'aliimi minassyaithoonirrojiim ",
+      "arti":
+          'Aku berlindung kepada Allah yang Maha Mendengar lagi Maha Mengetahui dari godaan syetan yang terkutuk.',
+    },
+    {
+      "no": 2,
+      "surah": "Surah Al-Fatihah",
+      "ayat": "1-7",
+      "dzikir":
+          "بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ ۝١ اَلْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِيْنَۙ ۝٢ الرَّحْمٰنِ الرَّحِيْمِۙ ۝٣ مٰلِكِ يَوْمِ الدِّيْنِۗ ۝٤ اِيَّاكَ نَعْبُدُ وَاِيَّاكَ نَسْتَعِيْنُۗ ۝٥ اِهْدِنَا الصِّرَاطَ الْمُسْتَقِيْمَۙ ۝٦ صِرَاطَ الَّذِيْنَ اَنْعَمْتَ عَلَيْهِمْ ەۙ غَيْرِ الْمَغْضُوْبِ عَلَيْهِمْ وَلَا الضَّاۤلِّيْنَࣖ ۝٧",
+      'latin':
+          "Bismillahirrohmaanirrohiim - Al hamdulillaahirobbil 'aalamiin - Ar rohmaanir  rohiim - Maaliki yaumid diin - Iyya ka na'budu wa iyya ka nasta 'iin -Iihdinash  shiroothol mustaqiim - Shirootol ladzina an'amta 'alaihim ghoiril magh dhuu  bi 'alaihim wa ladh dhool liin ",
+      "arti":
+          '(1) Dengan menyebut nama Allah Yang Maha Pemurah lagi Maha Penyayang. (2) Segala puji bagi Allah, Tuhan semesta alam. (3) Maha Pemurah lagi Maha Penyayang. (4) Yang menguasai di Hari Pembalasan. (5) Hanya Engkaulah yang kami sembah, dan hanya kepada Engkaulah kami meminta pertolongan. (6) Tunjukilah kami jalan yang lurus, (7) (yaitu) Jalan orang-orang yang telah Engkau beri nikmat kepada mereka; bukan (jalan) mereka yang dimurkai dan bukan (pula jalan) mereka yang sesat.',
+    },
+    {
+      "no": 3,
+      "surah": "Surah Al-Baqarah ayat 1-5",
+      "ayat": "1-5",
+      "dzikir":
+          " بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ -- الۤمّۤۚ ۝١ ذٰلِكَ الْكِتٰبُ لَا رَيْبَۛ فِيْهِۛ هُدًى لِّلْمُتَّقِيْنَۙ ۝٢ الَّذِيْنَ يُؤْمِنُوْنَ بِالْغَيْبِ وَيُقِيْمُوْنَ الصَّلٰوةَ وَمِمَّا رَزَقْنٰهُمْ يُنْفِقُوْنَۙ ۝٣ وَالَّذِيْنَ يُؤْمِنُوْنَ بِمَآ اُنْزِلَ اِلَيْكَ وَمَآ اُنْزِلَ مِنْ قَبْلِكَۚ وَبِالْاٰخِرَةِ هُمْ يُوْقِنُوْنَۗ ۝٤ اُولٰۤىِٕكَ عَلٰى هُدًى مِّنْ رَّبِّهِمْۙ وَاُولٰۤىِٕكَ هُمُ الْمُفْلِحُوْنَ ۝٥",
+      'latin':
+          "Bismillaahirrohmaanirrohiim - Alif Laam Miim - Dza likal kitaabu laaroi  bafiihi hudal lilmuttaqiin - Al ladziina yu'minuna bil ghoibi wa  yuqiimunassholaata wa mimma rozaqnaahum yum fiquun - Walladziina yu'  minuuna bimaa unzila ilaika wa maa unzila ming qoblika wa bil aakhiroti hum  yuu qinuun - Ulaa ika 'ala hudam mir  robbihim wa ulaa ika humul muflihuun",
+      "arti":
+          '(1) Alif Laam Miim. (2) Kitab (Al Qur’an) ini tidak ada keraguan padanya; petunjuk bagi mereka yang bertaqwa, (3) (yaitu) mereka yang beriman kepada yang gaib, yang mendirikan shalat dan menafkahkan sebahagian rezki yang Kami anugerahkan kepada mereka, (4) dan mereka yang beriman kepada Kitab (Al Qur’an) yang telah diturunkan kepadamu dan Kitab-kitab yang telah diturunkan sebelummu, serta mereka yakin akan adanya (kehidupan) akhirat. (5) Mereka itulah yang tetap mendapat petunjuk dari Tuhan mereka, dan merekalah orang-orang yang beruntung.',
+    },
+    {
+      "no": 4,
+      "surah": "Ayat Kursi",
+      "ayat": "255",
+      "dzikir":
+          "للّٰهُ لَآ اِلٰهَ اِلَّا هُوَۚ اَلْحَيُّ الْقَيُّوْمُ ەۚ لَا تَأْخُذُهٗ سِنَةٌ وَّلَا نَوْمٌۗ لَهٗ مَا فِى السَّمٰوٰتِ وَمَا فِى الْاَرْضِۗ مَنْ ذَا الَّذِيْ يَشْفَعُ عِنْدَهٗٓ اِلَّا بِاِذْنِهٖۗ يَعْلَمُ مَا بَيْنَ اَيْدِيْهِمْ وَمَا خَلْفَهُمْۚ وَلَا يُحِيْطُوْنَ بِشَيْءٍ مِّنْ عِلْمِهٖٓ اِلَّا بِمَا شَاۤءَۚ وَسِعَ كُرْسِيُّهُ السَّمٰوٰتِ وَالْاَرْضَۚ وَلَا يَـُٔوْدُهٗ حِفْظُهُمَاۚ وَهُوَ الْعَلِيُّ الْعَظِيْمُ ۝٢٥٥",
+      "latin":
+          "Alloohu laa ilaaha illa huwal hayyul qoyyuumu laa ta'khuzuhuu sinatuw walaa  naumun lahuu maa fissamaawaati wa maa fil ardhi man dzalladzii yasyfa'u  'indahu illa bi idznihi ya'lamu maa ba ina aidiihim wa maa kholfahum wa laa  yuhiithuuna bisyai-in min  'ilmihi illaa bi maa syaa -a wasi'a kursiyyuhus-samawaati wal ardhi wa laa yauuduhu hi fzuhuma wahuwal 'ali yyul 'azhiim.",
+      "arti":
+          'Allah, tidak ada Tuhan (yang berhak disembah) melainkan Dia Yang Hidup kekal lagi terus menerus mengurus (makhluk-Nya); tidak mengantuk dan tidak tidur. Kepunyaan-Nya apa yang di langit dan di bumi. Tiada yang dapat memberi syafaat di sisi Allah tanpa izin-Nya. Allah mengetahui apa-apa yang di hadapan mereka dan di belakang mereka, dan mereka tidak mengetahui apa-apa dari ilmu Allah melainkan apa yang dikehendaki-Nya. Kursi Allah meliputi langit dan bumi. Dan Allah tidak merasa berat memelihara keduanya, dan Allah Maha Tinggi lagi Maha Besar',
+    },
+    {
+      "no": 5,
+      "surah": "Surah Al-Baqarah ayat 256",
+      "ayat": "256",
+      "dzikir":
+          "لَآ اِكْرَاهَ فِى الدِّيْنِۗ قَدْ تَّبَيَّنَ الرُّشْدُ مِنَ الْغَيِّۚ فَمَنْ يَّكْفُرْ بِالطَّاغُوْتِ وَيُؤْمِنْۢ بِاللّٰهِ فَقَدِ اسْتَمْسَكَ بِالْعُرْوَةِ الْوُثْقٰى لَا انْفِصَامَ لَهَاۗ وَاللّٰهُ سَمِيْعٌ عَلِيْمٌ ۝٢٥٦",
+      "latin":
+          "La  ikroha fid-diini qot tabayyanar-rusydu minal ghoyyi famay yakfur bith-thooghuuti wa yu' minu billahi faqo dis tamsaka bil 'urwatil wutsqoo lan  fishooma laha wallohu samii'un aliim",
+      "arti":
+          'Tidak ada paksaan untuk (memasuki) agama (Islam); sesungguhnya telah jelas jalan yang benar daripada jalan yang sesat. Karena itu barang siapa yang ingkar kepada Thaghut dan beriman kepada Allah, maka sesungguhnya ia telah berpegang kepada buhu tali yang amat kuat yang tidak akan putus. Dan Allah Maha Mendengar lagi Maha Mengetahui.',
+    },
+  ]
+};
